416ab18ab811bbea2bc9504ff33fd5a78973ddf5
ehofas <t.kasnauskas@gmail.com>
2017-06-22 18:32:12 +0300

    changed default ports to 8082

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/dolphin'); // connect to our database
+//mongoose.connect('mongodb://localhost:27017/dolphin'); // connect to our database
 var player = new soundManager_1.default();
 // var Cat = mongoose.model('Cat', { name: String });
 //
@@ -26,11 +26,11 @@ var player = new soundManager_1.default();
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var port = process.env.PORT || 8080; // set our port
+var port = process.env.PORT || 8082; // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router(); // get an instance of the express Router
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// test route to make sure everything is working (accessed at GET http://localhost:8082/api)
 router.get('/', function (req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/dolphin'); // connect to our database
+#mongoose.connect('mongodb://localhost:27017/dolphin'); // connect to our database
 var player = new SoundManager();
 // var Cat = mongoose.model('Cat', { name: String });
 //
@@ -29,13 +29,13 @@ var player = new SoundManager();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8082;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// test route to make sure everything is working (accessed at GET http://localhost:8082/api)
 router.get('/', function (req, res) {
     res.json({message: 'hooray! welcome to our api!'});
 });
